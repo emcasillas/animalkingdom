0e2e9cc8ca735fd051a46290bea3a81b8e5efb54
emcasillas <97483258+emcasillas@users.noreply.github.com>
2022-02-28 19:39:43 -0600

    Animal Kingdom Visual Research Activity

---
+++ animalkingdom/gazelle/sketch.js
@@ -0,0 +1,11 @@
+//how to visually represent a habitat/nature scene in a website?
+function preload() {
+  img = loadImage('../background.jpg');
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  image(img, 0-(mouseX/10), 0-(mouseY/10), windowWidth+100, windowHeight+100);
+}
